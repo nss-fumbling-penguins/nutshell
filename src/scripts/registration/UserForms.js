6a2050708f388602f287580d19a07ae72c4f774d
joshdbarton <joshdbarton@gmail.com>
2018-05-15 14:53:36 -0500

    user forms module now working

--- src/scripts/registration/UserForms.js
+++ src/scripts/registration/UserForms.js
@@ -11,8 +11,9 @@ const APIManager = require("../api/APIManager")
 const userForms = Object.create({}, {
     buildSignUpForm: {
         value: () => {
+            $("#main-page").empty()
             $("#main-page").append(
-                `<section id="signup-form>
+                `<section id="signup-form">
                     <h2>Create a new Account:</h2>
                     <label>First Name:</label>
                     <input type="text" id="first-name">
@@ -23,50 +24,44 @@ const userForms = Object.create({}, {
                     <label>Choose a user name:</label>
                     <input type="text" id="username">
                     <label>Choose a Password:</label>
-                    <input type="text" id="password">
+                    <input type="password" id="password">
                     <input type="button" id="submit-button" value="Sign Up!">
                 </section>`
             )
             $("#submit-button").click(event => {
-                APIManager.getAllOfCollection("Users").then(users => {
-                    users.forEach(user => {
-                        if (user.username === $("#username").val()) {
-                            $("#signup-form").append("<p class='alert'>User name already taken, please choose another</p>")
-                        } else {
-                            userManager.createNewUser($("#first-name").val(), $("#last-name").val(), $("#username").val(), $("#password").val(), $("#email").val())
-                            APIManager.getAllOfCollection("Users").then(users => {
-                                const id = users.find(user => user.username === $("#username")).id
-                                userManager.logInUser(id)
-                            })
-                        }
+                const username = $("#username").val()
+                $.ajax(`http://localhost:8088/Users?username=${username}`).then(user => {     
+                        if (user.length === 0) {
+                            userManager.createNewUser(
+                                $("#first-name").val(), 
+                                $("#last-name").val(), 
+                                username, 
+                                $("#password").val(), 
+                                $("#email").val()
+                            )
+                    } else
+                        $("#signup-form").append("<p class='alert'>User name already taken, please choose another</p>") 
                     })
                 })
-            })
         }
-    }, 
+    },
     buildLoginForm: {
         value: () => {
+            $("#main-page").empty()
             $("#main-page").append(
-                `<section id="login-form>
+                `<section id="login-form">
                     <h2>Sign in:</h2>
                     <label>Username:</label>
                     <input type="text" id="username">
                     <label>Password:</label>
-                    <input type="text" id="password">
+                    <input type="password" id="password">
                     <input type="button" id="submit-button" value="Sign In">
                 </section>`
             )
             $("#submit-button").click(event => {
-                 const username =  $("#username").val()
-                 const password = $("#password").val()
-                 if ( userManager.authenticated($(username, password)) ) {
-                    APIManager.getAllOfCollection("Users").then(users => {
-                        const id = users.find(user => user.username === $("#username")).id
-                        userManager.logInUser(id)
-                        })  
-                 } else {
-                        alert("Username and/or password incorrect.  Please try again")
-                    }
+                const username =  $("#username").val()
+                const password = $("#password").val()
+                userManager.authenticate(username, password)                  
             })
         }
     }
