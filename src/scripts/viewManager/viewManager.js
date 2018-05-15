083c3ad16d834277ecbceeae4bf6085adb869e2f
joshdbarton <joshdbarton@gmail.com>
2018-05-15 15:54:27 -0500

    first commit

---
+++ src/scripts/viewManager/viewManager.js
@@ -0,0 +1,27 @@
+/*
+View Manager Module
+Author: Josh Barton
+This module builds the three views of the app: welcome, dashboard, and signed-out
+*/
+const $ = require("jquery")
+
+
+const viewManger = Object.create({}, {
+    buildLandingPage: {
+        value: () => {
+
+        }
+    }, 
+    buildDashboard: {
+        value: () => {
+
+        }
+    }, 
+    buildSignedOut: {
+        value: () => {
+
+        }
+    }
+})
+
+module.exports = viewManager
