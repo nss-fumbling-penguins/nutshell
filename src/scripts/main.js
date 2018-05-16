c5e057b9e4a3525978d14d8f5facb207b78935a1
Riley Mathews <rileymathews80@gmail.com>
2018-05-16 14:57:36 -0500

    moved function to load list into view manager

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,8 @@
 
 
 const viewManager = require("./viewManager/viewManager.js")
-const friendsList = require("./friends/friendsInit")
 
 const $ = require("jquery")
 
 viewManager.buildLandingPage()
-friendsList()
+
--- src/scripts/viewManager/viewManager.js
+++ src/scripts/viewManager/viewManager.js
@@ -6,6 +6,7 @@ This module builds the three views of the app: welcome, dashboard, and signed-ou
 const $ = require("jquery")
 const APIManager = require("../api/APIManager")
 const userForms = require("../registration/UserForms")
+const friendsList = require("../friends/friendsInit")
 
 const viewManager = Object.create({}, {
     buildLandingPage: {
@@ -54,6 +55,7 @@ const viewManager = Object.create({}, {
                 // function to build chat section
                 //function to build events
                 // function to build news
+                friendsList()
             })
 
         }
