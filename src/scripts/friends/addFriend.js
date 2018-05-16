3b0346272af2ab0778d47db8dc52a638630ac070
Riley Mathews <rileymathews80@gmail.com>
2018-05-16 11:39:59 -0500

    started add friend function

---
+++ src/scripts/friends/addFriend.js
@@ -0,0 +1,11 @@
+/* 
+    module to add a friend
+    Authors: Riley Mathews
+*/
+
+APIManager = require("../api/APIManager")
+UserManager = require("../registration/UserManager")
+
+const addFriend = (friendId) => {
+    
+}
