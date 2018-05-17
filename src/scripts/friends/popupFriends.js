76808dd7609d925de21a416fa8b46a6f9efa3b73
Riley Mathews <rileymathews80@gmail.com>
2018-05-17 11:07:10 -0500

    got popup friends list working

--- src/scripts/friends/popupFriends.js
+++ src/scripts/friends/popupFriends.js
@@ -4,13 +4,19 @@
 */
 
 const Popper = require("popper.js")
+const $ = require("jquery")
 
 const activatePopup = () => {
-    const ref = document.querySelector("#Show__Friends__List")
-    const pop = document.querySelector("#friends")
-    new Popper(ref, pop, {
-        modifiers: {
-            preventOverflow: { enabled: false }
+    const ref = $("#Show__Friends__List")
+    const pop = $("#friends")
+    new Popper(ref, pop, {})
+    pop.addClass("hide")
+    ref.click(() => {
+        pop.toggleClass("hide")
+        if (ref.text() === "Show Friends List") {
+            ref.text("Hide Friends List")
+        } else if (ref.text() === "Hide Friends List") {
+            ref.text("Show Friends List")
         }
     })
 }
