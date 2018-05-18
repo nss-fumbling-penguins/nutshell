90cf9879022981881fc904124866e5218a2eefbd
Riley Mathews <rileymathews80@gmail.com>
2018-05-18 12:39:36 -0500

    fixed error in add friend code

--- src/scripts/friends/addFriend.js
+++ src/scripts/friends/addFriend.js
@@ -12,20 +12,23 @@ const addFriend = (usernameToAdd) => {
     APIManager.getAllOfCollection("Users")
         .then(response => {
             const friendToAdd = response.find(user => user.username === usernameToAdd)
-            const friendId = friendToAdd.id
-            //get current user
-            const currentUserId = UserManager.currentUser()
-            if (currentUserId === friendId) {
-                alert("you can't add yourself as a friend")
-            } else {
-                friendCard = buildFriendCard(friendToAdd.id, friendToAdd.firstName, friendToAdd.lastName, friendToAdd.username)
-                $("#Friend__output").append(friendCard)
-                relationship = 
-                {
-                    "userID": currentUserId,
-                    "followID": friendId
+            if (friendToAdd !== undefined) {
+                console.log("got here")
+                const friendId = friendToAdd.id
+                //get current user
+                const currentUserId = UserManager.currentUser()
+                if (currentUserId === friendId) {
+                    alert("you can't add yourself as a friend")
+                } else {
+                    friendCard = buildFriendCard(friendToAdd.id, friendToAdd.firstName, friendToAdd.lastName, friendToAdd.username)
+                    $("#Friend__output").append(friendCard)
+                    relationship = 
+                    {
+                        "userID": currentUserId,
+                        "followID": friendId
+                    }
+                    APIManager.createItem("Relationships", relationship)
                 }
-                APIManager.createItem("Relationships", relationship)
             }
         })
 }
